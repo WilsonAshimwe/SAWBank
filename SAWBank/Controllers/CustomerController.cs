6a19311c4de816e7911923b93e91eba88b0530a2
ElOmari <elomari-adam@hotmail.com>
2024-01-25 11:27:12 +0100

    start customer controller

---
+++ SAWBank/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SAWBank.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] )
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult SoftDelete() 
+        {  
+            throw new NotImplementedException(); 
+        }
+
+        [HttpGet] public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ SAWBank/DTO/Customer/RegisterCustomerDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAWBank.API.DTO.CustomerDTO
+{
+    public class RegisterCustomerDTO
+    {
+        [Required]
+        public string Username { get; set; }
+    }
+}
